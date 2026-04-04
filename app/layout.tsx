64dd5ce523039fd2272874675ad4ad46ac425d50
sid <siddharth.gntrdy@gmail.com>
2026-04-04 15:19:03 -0400

    back to datelyne spelled with a y for all the tik tok wooman

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "DATELEINE",
+  title: "DATELYNE",
   description: "Emergency callback system.",
 };
 
